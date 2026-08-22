496d5efd389d37b3a6a23ac58cb791a5703416c2
senecursos <cursos.sene@gmail.com>
2026-08-22 17:53:50 -0300

    Add files via upload

---
+++ analytics.js
@@ -0,0 +1,14 @@
+// Arquivo Central de Métricas - Português Estratégico
+
+// 1. Carrega o script oficial do Google Analytics
+var script = document.createElement('script');
+script.async = true;
+script.src = 'https://www.googletagmanager.com/gtag/js?id=G-QK1TRGPE1F';
+document.head.appendChild(script);
+
+// 2. Inicializa as configurações de rastreamento
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-QK1TRGPE1F');
